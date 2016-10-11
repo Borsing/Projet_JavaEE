e3e18a943d3a1cce2d4ebbbce829b43d7d483bc3
Adrien Cadoret <adrien cadoret>
2016-10-11 16:24:05 +0200

    V2 of Adding UML Diagram, Interfaces and Handler

--- src/main/java/servlet/FrontControllerServlet.java
+++ src/main/java/servlet/FrontControllerServlet.java
@@ -1,11 +1,14 @@
 package servlet;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by adric on 07/10/2016.
@@ -14,14 +17,20 @@ import java.io.PrintWriter;
  */
 public class FrontControllerServlet extends HttpServlet{
 
-
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        resp.setContentType("text/html");
-
+        RequestDispatcher rd ;
+        ServletContext context;
         // TODO Split the request with the @RequestHandlerInterface (/user, /event, /participant)
+
         // TODO Call the right process (@EventController or @ParticipantController or @UserController
         // TODO Forward to the template to change the body of the page
+        context = getServletContext();
+        rd = context.getRequestDispatcher("/layout/template.jsp"); // Do not forward to another jsp
+        if(rd != null){
+            rd.forward(req,resp);
+        }
 
     }
 
